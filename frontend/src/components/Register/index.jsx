7ccd337c2f0e1b3b1134b3d42305675411b8b55b
Jvargast <jose.vargas.t@csi.cl>
2021-12-10 18:10:32 -0300

    vistas integradas

---
+++ frontend/src/components/Register/index.jsx
@@ -0,0 +1,297 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import {
+  AcmeNormalWhite50px,
+  RobotoMediumPictonBlue22px,
+  RobotoLightTurquoisePearl14px,
+  RobotoLightBlack25px,
+  AdobeblankRegularNormalWhite25px,
+  OffsideNormalWhite26px,
+  ValignTextBottom,
+  RobotoLightWhite22px,
+  RobotoLightBlack22px,
+} from "../../styledMixins";
+import "./Register.css";
+
+class Register extends React.Component {
+  render() {
+    const {
+      usuario,
+      usuarioCopy,
+      usuarioCopy3,
+      usuarioCopy4,
+      contenedorLogo,
+      usuarioCopy5,
+      contenedorLadoDerecho,
+      usuarioCopy6,
+      usuarioCopy2,
+      regstrate,
+      nuestroDeseo,
+      intercambiaTuLibroPorUnoD,
+      contrasea,
+      noTienesCuenta,
+      registrate,
+      regstrate2,
+      intercambiaTuLibro,
+    } = this.props;
+
+    return (
+      <div className="container-center-horizontal">
+        <div className="register screen">
+          <OverlapGroup>
+            <Contenedor></Contenedor>
+            <Rectangle2 src="/img/rectangle-2-2@2x.svg" />
+            <Rectangle></Rectangle>
+            <RectangleCopy2></RectangleCopy2>
+            <RectangleCopy3></RectangleCopy3>
+            <RectangleCopy></RectangleCopy>
+            <Usuario>{usuario}</Usuario>
+            <UsuarioCopy>{usuarioCopy}</UsuarioCopy>
+            <UsuarioCopy3>{usuarioCopy3}</UsuarioCopy3>
+            <UsuarioCopy4>{usuarioCopy4}</UsuarioCopy4>
+            <ContenedorLogo src={contenedorLogo} />
+            <UsuarioCopy5>{usuarioCopy5}</UsuarioCopy5>
+            <ContenedorLadoDerecho src={contenedorLadoDerecho} />
+            <UsuarioCopy6>{usuarioCopy6}</UsuarioCopy6>
+            <UsuarioCopy2>{usuarioCopy2}</UsuarioCopy2>
+            <Regstrate>{regstrate}</Regstrate>
+            <NuestroDeseo>{nuestroDeseo}</NuestroDeseo>
+            <IntercambiaTuLibroPorUnoD>{intercambiaTuLibroPorUnoD}</IntercambiaTuLibroPorUnoD>
+            <Contrasea>{contrasea}</Contrasea>
+            <NoTienesCuenta>{noTienesCuenta}</NoTienesCuenta>
+            <Link to="/login">
+              <Registrate>{registrate}</Registrate>
+            </Link>
+            <Link to="/home-page">
+              <Regstrate1>{regstrate2}</Regstrate1>
+            </Link>
+            <IntercambiaTuLibro>{intercambiaTuLibro}</IntercambiaTuLibro>
+          </OverlapGroup>
+        </div>
+      </div>
+    );
+  }
+}
+
+const OverlapGroup = styled.div`
+  width: 1920px;
+  height: 1080px;
+  position: relative;
+  background-image: url(/img/background-1@1x.svg);
+  background-size: 100% 100%;
+`;
+
+const Contenedor = styled.div`
+  position: absolute;
+  width: 1000px;
+  height: 600px;
+  top: 240px;
+  left: 460px;
+  background-color: var(--white);
+`;
+
+const Rectangle2 = styled.img`
+  position: absolute;
+  width: 352px;
+  height: 50px;
+  top: 694px;
+  left: 509px;
+`;
+
+const Rectangle = styled.div`
+  position: absolute;
+  width: 352px;
+  height: 1px;
+  top: 467px;
+  left: 509px;
+  background-color: var(--quill-gray);
+`;
+
+const RectangleCopy2 = styled.div`
+  position: absolute;
+  width: 352px;
+  height: 1px;
+  top: 537px;
+  left: 509px;
+  background-color: var(--quill-gray);
+`;
+
+const RectangleCopy3 = styled.div`
+  position: absolute;
+  width: 352px;
+  height: 1px;
+  top: 607px;
+  left: 509px;
+  background-color: var(--quill-gray);
+`;
+
+const RectangleCopy = styled.div`
+  position: absolute;
+  width: 352px;
+  height: 1px;
+  top: 667px;
+  left: 509px;
+  background-color: var(--quill-gray);
+`;
+
+const Usuario = styled.div`
+  ${ValignTextBottom}
+  ${RobotoLightBlack22px}
+            position: absolute;
+  width: 103px;
+  height: 22px;
+  top: 412px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const UsuarioCopy = styled.div`
+  ${RobotoLightTurquoisePearl14px}
+  position: absolute;
+  width: 117px;
+  top: 443px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const UsuarioCopy3 = styled.div`
+  ${ValignTextBottom}
+  ${RobotoLightBlack22px}
+            position: absolute;
+  width: 103px;
+  height: 22px;
+  top: 482px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const UsuarioCopy4 = styled.div`
+  ${RobotoLightTurquoisePearl14px}
+  position: absolute;
+  width: 117px;
+  top: 513px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const ContenedorLogo = styled.img`
+  position: absolute;
+  width: 249px;
+  height: 40px;
+  top: 280px;
+  left: 561px;
+  object-fit: cover;
+`;
+
+const UsuarioCopy5 = styled.div`
+  ${ValignTextBottom}
+  ${RobotoLightBlack22px}
+            position: absolute;
+  width: 103px;
+  height: 22px;
+  top: 552px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const ContenedorLadoDerecho = styled.img`
+  position: absolute;
+  width: 550px;
+  height: 600px;
+  top: 240px;
+  left: 910px;
+  object-fit: cover;
+`;
+
+const UsuarioCopy6 = styled.div`
+  ${RobotoLightTurquoisePearl14px}
+  position: absolute;
+  width: 117px;
+  top: 583px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const UsuarioCopy2 = styled.div`
+  ${RobotoLightTurquoisePearl14px}
+  position: absolute;
+  width: 75px;
+  top: 647px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const Regstrate = styled.div`
+  ${RobotoLightBlack25px}
+  position: absolute;
+  top: 343px;
+  left: 629px;
+  letter-spacing: 0;
+`;
+
+const NuestroDeseo = styled.div`
+  ${AcmeNormalWhite50px}
+  position: absolute;
+  width: 364px;
+  top: 316px;
+  left: 969px;
+  letter-spacing: 0;
+`;
+
+const IntercambiaTuLibroPorUnoD = styled.div`
+  ${AdobeblankRegularNormalWhite25px}
+  position: absolute;
+  top: 431px;
+  left: 969px;
+  letter-spacing: 0;
+`;
+
+const Contrasea = styled.div`
+  ${RobotoLightBlack22px}
+  position: absolute;
+  width: 113px;
+  top: 622px;
+  left: 509px;
+  letter-spacing: 0;
+`;
+
+const NoTienesCuenta = styled.div`
+  ${RobotoLightBlack22px}
+  position: absolute;
+  width: 186px;
+  top: 789px;
+  left: 521px;
+  text-align: right;
+  letter-spacing: 0;
+`;
+
+const Registrate = styled.div`
+  ${RobotoMediumPictonBlue22px}
+  position: absolute;
+  width: 138px;
+  top: 789px;
+  left: 711px;
+  letter-spacing: 0;
+  cursor: pointer;
+`;
+
+const Regstrate1 = styled.div`
+  ${RobotoLightWhite22px}
+  position: absolute;
+  top: 706px;
+  left: 636px;
+  letter-spacing: 0;
+  cursor: pointer;
+`;
+
+const IntercambiaTuLibro = styled.div`
+  ${OffsideNormalWhite26px}
+  position: absolute;
+  width: 465px;
+  top: 423px;
+  left: 952px;
+  letter-spacing: 0;
+`;
+
+export default Register;
---
+++ frontend/src/styledMixins.js
@@ -0,0 +1,159 @@
+import { css } from "styled-components";
+
+export const ValignTextBottom = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+export const RobotoLightBlack22px = css`
+  color: var(--black);
+  font-family: var(--font-family-roboto);
+  font-size: var(--font-size-xxs);
+  font-weight: 300;
+  font-style: normal;
+`;
+
+export const BarlowNormalEerieBlack18px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-barlow);
+  font-size: var(--font-size-xxxs);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const RobotoLightTurquoisePearl14px = css`
+  color: var(--turquoise-pearl);
+  font-family: var(--font-family-roboto);
+  font-size: 14px;
+  font-weight: 300;
+  font-style: normal;
+`;
+
+export const InterNormalEerieBlack24px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-inter);
+  font-size: 24px;
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const InterNormalBlack18px = css`
+  color: var(--black);
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-xxxs);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const InterNormalEerieBlack18px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-xxxs);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const NobileBoldEerieBlack48px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-nobile);
+  font-size: var(--font-size-l);
+  font-weight: 700;
+  font-style: normal;
+`;
+
+export const InterNormalEerieBlack16px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-inter);
+  font-size: 16px;
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const NobileNormalEerieBlack66px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-nobile);
+  font-size: var(--font-size-xl);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const AlataNormalEerieBlack27px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-alata);
+  font-size: var(--font-size-m);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const NobileBoldEerieBlack27px = css`
+  color: var(--eerie-black);
+  font-family: var(--font-family-nobile);
+  font-size: var(--font-size-m);
+  font-weight: 700;
+  font-style: normal;
+`;
+
+export const InterNormalAlto18px = css`
+  color: var(--alto);
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-xxxs);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const RobotoLightBlack25px = css`
+  color: var(--black);
+  font-family: var(--font-family-roboto);
+  font-size: var(--font-size-xs);
+  font-weight: 300;
+  font-style: normal;
+`;
+
+export const AcmeNormalWhite50px = css`
+  color: var(--white);
+  font-family: var(--font-family-acme);
+  font-size: 50px;
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const AdobeblankRegularNormalWhite25px = css`
+  color: var(--white);
+  font-family: var(--font-family-adobe_blank-regular);
+  font-size: var(--font-size-xs);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const RobotoMediumPictonBlue22px = css`
+  color: var(--picton-blue);
+  font-family: var(--font-family-roboto);
+  font-size: var(--font-size-xxs);
+  font-weight: 500;
+  font-style: normal;
+`;
+
+export const RobotoLightWhite22px = css`
+  color: var(--white);
+  font-family: var(--font-family-roboto);
+  font-size: var(--font-size-xxs);
+  font-weight: 300;
+  font-style: normal;
+`;
+
+export const OffsideNormalWhite26px = css`
+  color: var(--white);
+  font-family: var(--font-family-offside);
+  font-size: var(--font-size-s);
+  font-weight: 400;
+  font-style: normal;
+`;
+
+export const Border1pxBlack = css`
+  border: 1px solid var(--black);
+`;
+
+export const Border1pxBlack2 = css`
+  border: 1px solid var(--black-2);
+`;
